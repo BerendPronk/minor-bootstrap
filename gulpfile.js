1cd7c9a9181f1da4abcfd2db1f2a7edf4c5934d8
Berend Pronk <berendpronk@quicknet.nl>
2017-03-15 16:43:39 +0100

    Adds gulpfile to bundle JavaScript

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp        = require('gulp'),
+    minify      = require('gulp-minifier'),
+    concatJs      = require('gulp-concat');
+    concatCss   = require('gulp-concat-css');
+
+
+/* ============================================================
+    Main tasks
+     ============================================================ */
+
+gulp.task('default', ['minifyJs']);
+
+/* ============================================================
+    Configuration
+     ============================================================ */
+
+var config = {
+    assetsPath: 'src/assets',
+    debug: true
+};
+
+var handleError = function(err) {
+    gutil.log(err);
+    this.emit('end')
+};
+
+/* ============================================================
+    Concatenate
+     ============================================================ */
+
+gulp.task('concatCss', function () {
+    return gulp.src(config.assetsPath + '/css/src/*.css')
+        .pipe(concatCss('bundle.css'))
+        .pipe(gulp.dest(config.assetsPath + '/css/src'));
+});
+
+gulp.task('concatJs', function() {
+  return gulp.src(['./src/assets/js/vendor/jquery.min.js', './src/dist/js/bootstrap.js', './src/assets/js/docs.min.js', './src/assets/js/ie10-viewport-bug-workaround.js'])
+    .pipe(concatJs('bundle.js'))
+    .pipe(gulp.dest(config.assetsPath + '/js/src'));
+});
+
+/* ============================================================
+    Minify
+     ============================================================ */
+
+gulp.task('minifyCss', function() {
+    return gulp.src(config.assetsPath + '/css/src/bundle.css').pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: false,
+    minifyCSS: true,
+    getKeptComment: function (content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+    }
+    })).pipe(gulp.dest('src/dist/css'));
+});
+
+gulp.task('minifyJs', function() {
+    return gulp.src(config.assetsPath + '/js/src/bundle.js').pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: false,
+    getKeptComment: function (content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+    }
+    })).pipe(gulp.dest('src/dist/js'));
+});
+
